3d0533ff15750a81a34e97414cf3f21f7d51341a
Moses Ugwulo <74902008+MosesUgwulo@users.noreply.github.com>
2022-12-07 13:06:16 +0000

    Topic 11 - I've completed all my labs!

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -3,6 +3,7 @@ import { movies, movieReviews, movieDetails } from './moviesData';
 import uniqid from 'uniqid';
 import movieModel from './movieModel';
 import asyncHandler from 'express-async-handler';
+import getUpcomingMovies from '../tmdb-api';
 
 const router = express.Router(); 
 
@@ -54,3 +55,8 @@ router.post('/:id/reviews', (req, res) => {
 });
 
 export default router;
+
+router.get('/tmdb/upcoming', asyncHandler( async(req, res) => {
+    const upcomingMovies = await getUpcomingMovies();
+    res.status(200).json(upcomingMovies);
+  }));
